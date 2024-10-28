499446eebb71fcfc0808edb564ce32399d10c1fa
cevheri <cevheribozoglan@gmail.com>
2024-10-28 20:32:23 +0300

    set log level

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,16 @@ print("logger configuration")
 logging.basicConfig(level=log_settings.LOG_LEVEL,
                     format=log_settings.LOG_FORMAT,
                     handlers=[logging.FileHandler(log_settings.LOG_FILE), logging.StreamHandler()])
-logging.getLogger('pymongo').setLevel(logging.INFO)
+logging.getLogger("pymongo").setLevel(logging.WARN)
+logging.getLogger("uvicorn").setLevel(logging.WARN)
+logging.getLogger("pydantic").setLevel(logging.WARN)
+logging.getLogger("fastapi").setLevel(logging.WARN)
+logging.getLogger("beanie").setLevel(logging.WARN)
+logging.getLogger("urllib3").setLevel(logging.WARN)
+logging.getLogger("passlib").setLevel(logging.WARN)
+logging.getLogger("starlette").setLevel(logging.WARN)
+logging.getLogger("bitsandbytes").setLevel(logging.WARN)
+logging.getLogger("sentence_transformers").setLevel(logging.WARN)
 log = logging.getLogger(__name__)
 # endregion logger configuration
 
