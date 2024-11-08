e665571f79a052c4ab9d6df5e75d33e5dad8a91e
cevheri <cevheribozoglan@gmail.com>
2024-11-08 04:23:28 +0300

    feat: logging improvement (console, file, and db). File : time and size rotation

--- app/conf/env/log_config.py
+++ app/conf/env/log_config.py
@@ -13,8 +13,23 @@ class LoggingSettings(BaseSettings):
         Logging level
     LOG_FILE: str
         Logging file
+    LOG_FORMAT: str
+        Logging format string for log messages like timestamp, name, level, message
+    LOG_HANDLER: list[str]
+        Logging handlers like console, file, db
+    LOG_BACKUP_COUNT: int 7
+        Log file will be rotated after 7 files
+    LOG_MAX_DAYS: int 7
+        Log file will be rotated after 7 days
+    LOG_MAX_SIZE: int 10MB
+        Log file will be rotated after 10MB
     """
 
     LOG_LEVEL: str = "DEBUG"
     LOG_FILE: str = "/tmp/app.log"
     LOG_FORMAT: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    LOG_HANDLER: list[str] = ["console", "file", "db"]
+    LOG_BACKUP_COUNT: int = 7 # log file will be rotated after 7 files
+    LOG_MAX_DAYS: int = 7 # log file will be rotated after 7 days
+    LOG_MAX_SIZE: int = (10 * 1024 * 1024)  # 10MB log file will be rotated after 10MB
+
--- app/middleware/log_handler.py
+++ app/middleware/log_handler.py
@@ -1,24 +1,85 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
 
 from log4mongo.handlers import MongoHandler
 
 from app.conf.app_settings import log_settings, app_settings, db_settings
 
-def init_log():
-    _handlers = [logging.FileHandler(log_settings.LOG_FILE),
-                 logging.StreamHandler(),
-                 MongoHandler(host=db_settings.HOST,
-                             port=db_settings.PORT,
-                             username=db_settings.USERNAME,
-                             password=db_settings.PASSWORD,
-                             database_name=db_settings.DATABASE_NAME,
-                             collection=db_settings.LOG_COLLECTION,
-                             )
-                 ]
 
-    logging.basicConfig(level=log_settings.LOG_LEVEL,
-                        format=log_settings.LOG_FORMAT,
-                        handlers=_handlers)
+def get_mongodb_handler():
+    mongodb_handler = MongoHandler(host=db_settings.HOST,
+                                   port=db_settings.PORT,
+                                   username=db_settings.USERNAME,
+                                   password=db_settings.PASSWORD,
+                                   database_name=db_settings.DATABASE_NAME,
+                                   collection=db_settings.LOG_COLLECTION,
+                                   )
+    # mongodb_handler.setLevel(log_settings.LOG_LEVEL)
+    # mongodb_handler.setFormatter(logging.Formatter(log_settings.LOG_FORMAT))
+    return mongodb_handler
+
+
+def get_console_handler():
+    console_handler = logging.StreamHandler()
+    # console_handler.setLevel(log_settings.LOG_LEVEL)
+    console_handler.setFormatter(logging.Formatter(log_settings.LOG_FORMAT))
+    return console_handler
+
+
+def get_file_time_rotation_handler():
+    time_rotation_handler = TimedRotatingFileHandler(
+        filename=log_settings.LOG_FILE,
+        when="midnight",
+        interval=1,
+        backupCount=log_settings.LOG_MAX_DAYS,
+        encoding="utf-8",
+        delay=False,
+        utc=True,
+        atTime=None,
+        errors=None)
+    # time_rotation_handler.setLevel(log_settings.LOG_LEVEL)
+    # time_rotation_handler.setFormatter(logging.Formatter(log_settings.LOG_FORMAT))
+    return time_rotation_handler
+
+
+def get_file_size_rotation_handler():
+    size_rotation_handler = RotatingFileHandler(
+        filename=log_settings.LOG_FILE,
+        mode="a",
+        maxBytes=log_settings.LOG_MAX_SIZE,
+        backupCount=log_settings.LOG_BACKUP_COUNT,
+        encoding="utf-8",
+        delay=False,
+        errors=None)
+    # size_rotation_handler.setLevel(log_settings.LOG_LEVEL)
+    # size_rotation_handler.setFormatter(logging.Formatter(log_settings.LOG_FORMAT))
+    return size_rotation_handler
+
+
+def configure_handler():
+    _handlers = []
+
+    if "db" in log_settings.LOG_HANDLER:
+        _handlers.append(get_mongodb_handler())
+
+    if "file" in log_settings.LOG_HANDLER:
+        _handlers.append(get_file_time_rotation_handler())
+        _handlers.append(get_file_size_rotation_handler())
+
+    if "console" in log_settings.LOG_HANDLER or not _handlers:
+        _handlers.append(get_console_handler())
+
+    return _handlers
+
+
+def init_log():
+    logging.basicConfig(
+        level=log_settings.LOG_LEVEL,
+        format=log_settings.LOG_FORMAT,
+        handlers=configure_handler()
+    )
+
+    logging.getLogger("asyncio").setLevel(logging.WARN)
     logging.getLogger("pymongo").setLevel(logging.WARN)
     logging.getLogger("uvicorn").setLevel(logging.WARN)
     logging.getLogger("pydantic").setLevel(logging.WARN)
